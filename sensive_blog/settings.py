3f24c172f0a384a178447f1c0b12deb5a82c0b37
tomaa-dev <lajpanovatoma@gmail.com>
2026-06-24 20:51:24 +0300

    Setup debug toolbar

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'debug_toolbar',
     'blog',
 ]
 
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'sensive_blog.urls'
@@ -99,3 +100,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 from django.conf.urls.static import static
 from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('tag/<slug:tag_title>', views.tag_filter, name='tag_filter'),
     path('contacts/', views.contacts, name='contacts'),
     path('', views.index, name='index'),
-]
+] + debug_toolbar_urls()
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
